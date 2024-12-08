b5bc92f0d6727422ebbae4a366ae9901b690dfc0
Benceszalaiii <szalai.bence.bendeguz@students.jedlik.eu>
2024-12-08 17:26:08 +0100

    Kockak

---
+++ kockak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace kockak
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hány alkalommal legyen feldobás?");
+            int hanyszor = Convert.ToInt32(Console.ReadLine());
+            int Anni = 0;
+            int Panni = 0;
+            Random rand = new Random();
+            for (int i = 0; i < hanyszor; i++)
+            {
+                string nyertes = "";
+                int[] dobasok = new int[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    dobasok[j] = rand.Next(1, 7); // Also inclusive, Felso exclusive (1 benne van, 7 nincs)
+                }
+                if (dobasok.Sum() >= 10)
+                {
+                    Panni++;
+                    nyertes = "Panni";
+                }
+                else
+                {
+                    Anni++;
+                    nyertes = "Anni";
+                }
+
+                Console.WriteLine($"Dobás: {dobasok[0]} + {dobasok[1]} + {dobasok[2]} = {dobasok.Sum()} Nyert: {nyertes}");
+            }
+
+            Console.WriteLine($"A játék során {Anni} alkalommal Anni, 3 alkalommal {Panni} nyert.");
+            Console.ReadKey();
+        }
+    }
+}
